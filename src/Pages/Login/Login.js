6a800788a6f6a59d8f6ba5347375fee0a7320f44
mkpbd <mostofakamal1990@gmail.com>
2022-05-24 21:02:46 +0600

    regitster form validation Ok

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,76 +1,99 @@
-import React from 'react';
-import { Row } from 'react-bootstrap';
-import { FaFacebook, FaGithub, FaGoogle} from 'react-icons/fa';
+import React from "react";
+import { Row } from "react-bootstrap";
+import { FaFacebook, FaGithub, FaGoogle } from "react-icons/fa";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
-    return (
-        <div className="limiter">
-		<div className="container-login100" style={{"background-image": "url('images/bg-01.jpg')"}}>
-			<div className="wrap-login100 p-l-110 p-r-110 p-t-62 p-b-33">
-				<form className="login100-form validate-form flex-sb flex-w p-5">
-					<span className="login100-form-title p-b-53 mb-4">
-						Sign In With
-					</span>
-                    <Row className='d-flex px-5 '>
-                        
-					<a href="#" className="btn-face m-b-20 me-3">
-        <FaGithub className='h2 me-2'></FaGithub>
-						Github
-					</a>
+  const { register,formState: { errors }, handleSubmit } = useForm();
 
-					<a href="#" className="btn-google m-b-20 bg-primary text-light">
-                    <FaGoogle className='h2 me-2 text-warning'></FaGoogle>
-						Google
-					</a>
+  const onSubmitForm = (data) => {
+
+	console.log(data)
+  };
+
+  return (
+    <div className="limiter">
+      <div
+        className="container-login100"
+        style={{ "backgroundImage": "url('images/bg-01.jpg')" }}
+      >
+        <div className="wrap-login100 p-l-110 p-r-110 p-t-62 p-b-33">
+        
+            <span className="login100-form-title p-b-53 mb-4 pt-3">
+              Sign In With
+            </span>
+            <Row className="d-flex px-5 ">
+              <a href="#" className="btn-face m-b-20 me-3">
+                <FaGithub className="h2 me-2"></FaGithub>
+                Github
+              </a>
+
+              <a href="#" className="btn-google m-b-20 bg-primary text-light">
+                <FaGoogle className="h2 me-2 text-warning"></FaGoogle>
+                Google
+              </a>
+            </Row>
+			<form className="login100-form validate-form flex-sb flex-w p-5" onSubmit={handleSubmit(onSubmitForm)}>
+              <div className="p-t-31 p-b-9 my-2">
+                <label htmlFor="userName" className="txt1">
+                  Username
+                </label>
+              </div>
+              <div
+                className="wrap-input100 validate-input"
+                data-validate="Username is required"
+              >
+                <input
+                  className="input100"
+                  type="text"
+				  {...register('username',{required : true, pattern : /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/})}
+                  name="username"
+                  id="userName"
+                />
+                <span className="focus-input100"></span>
+              </div>
+			  {errors.username && <p className="text-danger">Email Address  invalid </p>}
+
+
+              <div className="p-t-13 p-b-9 my-3">
+                <label htmlFor="password" className="txt1">
+                  Password
+                  <a href="#" className="txt2 bo1 m-l-5 my-2 ms-2">
+                    Forgot?
+                  </a>
+                </label>
+              </div>
+              <div
+                className="wrap-input100 validate-input"
+                data-validate="Password is required"
+              >
+                <input
+                  className="input100"
+                  type="password"
+                  {...register("password",{required: true, minLength:6})}
+                  id="password"
+                />
+                <span className="focus-input100"></span>
 				
-                    </Row>
-					
-					<div className="p-t-31 p-b-9 my-2">
-						<label htmlFor='userName' className="txt1">
-							Username
-						</label>
-					</div>
-					<div className="wrap-input100 validate-input" data-validate = "Username is required">
-						<input className="input100" type="text" name="username" id="userName" />
-						<span className="focus-input100"></span>
-					</div>
-					
-					<div className="p-t-13 p-b-9 my-3">
-						<label htmlFor='password' className="txt1">
-							Password
-                            <a href="#" className="txt2 bo1 m-l-5 my-2 ms-2">
-							Forgot?
-						</a>
-						</label>
+              </div>
+			  {errors.password && <p className="text-danger">Password invalid or at list 6 character </p>}
 
-					
-					</div>
-					<div className="wrap-input100 validate-input" data-validate = "Password is required">
-						<input className="input100" type="password" name="password" id='password' />
-						<span className="focus-input100"></span>
-					</div>
+              <div className="container-login100-form-btn m-t-17 my-2">
+                <button className="login100-form-btn">Sign In</button>
+              </div>
+            </form>
+            <div className="w-full text-center p-t-55 my-3">
+              <span className="txt2">Not a member?</span>
 
-					<div className="container-login100-form-btn m-t-17 my-2">
-						<button className="login100-form-btn">
-							Sign In
-						</button>
-					</div>
-
-					<div className="w-full text-center p-t-55 my-3">
-						<span className="txt2">
-							Not a member?
-						</span>
-
-						<a href="#" className="txt2 bo1 ms-2">
-							Sign up now
-						</a>
-					</div>
-				</form>
-			</div>
-		</div>
-	</div>
-	
-    );
+              <a href="#" className="txt2 bo1 ms-2">
+                Sign up now
+              </a>
+            </div>
+        
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import { Row } from 'react-bootstrap';
+import { useForm,Controller } from 'react-hook-form';
 import { FaGithub, FaGoogle } from 'react-icons/fa';
 import './Register.css'
 
 const Register = () => {
+
+	const { register,formState: { errors }, handleSubmit ,control, watch} = useForm();
+
+	const onSubmitForm = (data) => {
+  
+	  console.log(data)
+	};
+
+	const password = watch('password');
+
     return (
         <div className="limiter">
-		<div className="container-login100" style={{"background-image": "url('images/bg-01.jpg')"}}>
+		<div className="container-login100" style={{"backgroundImage": "url('images/bg-01.jpg')"}}>
 			<div className="wrap-login100 p-l-110 p-r-110 p-t-62 p-b-33">
-				<form className="login100-form validate-form flex-sb flex-w p-5">
+				<form onSubmit={handleSubmit(onSubmitForm)} className="login100-form validate-form flex-sb flex-w p-5">
 					<span className="login100-form-title p-b-53 mb-4">
 						Register Form
 					</span>
@@ -33,9 +44,10 @@ const Register = () => {
 						</label>
 					</div>
 					<div className="wrap-input100 validate-input" data-validate = "Username is required">
-						<input className="input100" type="text" name="username" id="userName" />
+						<input className="input100" type="text" {...register("username",{required:true, minLength:4})} id="userName" />
 						<span className="focus-input100"></span>
 					</div>
+					{errors.username && <p className='text-danger'>user name required at list 4 character  or more </p>}
                     </Row>
 					
 					
@@ -46,9 +58,10 @@ const Register = () => {
 						</label>
 					</div>
 					<div className="wrap-input100 validate-input" data-validate = "email is required">
-						<input className="input100" type="text" name="email" id="email" />
+						<input className="input100" type="text" {...register('email', {required : true, pattern:   /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/})} id="email" />
 						<span className="focus-input100"></span>
 					</div>
+					{errors.email && <p className='text-danger'>Email required </p>}
                     </Row>
 
                   
@@ -58,13 +71,12 @@ const Register = () => {
                     <div className="p-t-13 p-b-9 my-3">
 						<label htmlFor='password' className="txt1">	Password</label>
 
-					
 					</div>
 					<div className="wrap-input100 validate-input" data-validate = "Password is required">
-						<input className="input100" type="password" name="password" id='password' />
+						<input className="input100" type="password" {...register('password', {required: true, minLength: 6})} id='password' />
 						<span className="focus-input100"></span>
 					</div>
-
+					{errors.password && <p className='text-danger'>password required and 6 or more character </p>}
                     <Row>
                     <div className="p-t-31 p-b-9 my-2">
 						<label htmlFor='confirmPassword' className="txt1">
@@ -72,9 +84,11 @@ const Register = () => {
 						</label>
 					</div>
 					<div className="wrap-input100 validate-input" data-validate = "email is required">
-						<input className="input100" type="password" name="confirmPassword" id="confirmPassword" />
+						<input className="input100" type="password" {...register('confirmPassword', {required: true, minLength: 6})} id="confirmPassword" />
 						<span className="focus-input100"></span>
 					</div>
+					{errors.confirmPassword && <p className='text-danger'>Confirm password required and 6 or more character </p>}
+					{errors.password !== errors.confirmPassword && <p className='text-danger'>password and confirm password dose not match </p>}
                     </Row>
                     </Row>
 
